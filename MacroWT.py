d2b601c794adc8158e696bc8c53d49dc63a28fa1
Ryan Romero <nayr77@gmail.com>
2021-01-17 19:11:11 -0700

    Adding a piece of code to write to a file.

--- MacroWT.py
+++ MacroWT.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+dateWT = input("Enter the date needed for input (mm/dd/yy): ")
 currWt = float(input("Enter current weight: "))
 
-print("My current weight is ",currWt, ".")
+f = open('output', 'w')
+f.write(str(dateWT)  + '\n')
+f.write(str(currWt)  + '\n')
+f.close()
+
+print("On",dateWT,"my weight was",currWt,".")
